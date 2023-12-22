13e1e36b2ddbc2e6221a9608594ad36c50c8f8d2
r2hu1 <87074472+r2hu1@users.noreply.github.com>
2023-12-22 16:41:39 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
         <footer className="-mt-10 py-10 grid gap-3 px-8 md:px-20 border-t">
             <ul>
                 <li>
-                    <a className="text-md hover:underline hover:text-primary" href="#">Fix this page</a>
+                    <a className="text-md hover:underline hover:text-primary" href="https://github.com/r2hu1/fquotes/">Fix this page</a>
                 </li>
                 <li>
                     <a className="text-md hover:underline hover:text-primary" href="https://rahul.eu.org">Hire me</a>
